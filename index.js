36fef96279d31041fc055c4a8cdc014a2520b0b9
Jannis R <mail@jannisr.de>
2016-02-22 22:09:26 +0100

    implementation :clap:

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const connect =    require('stream-connect')
+const lineLength = require('line-length-stream')
+const through =    require('through2')
+const wrap =       require('wrap-stream')
+
+
+
+const head = '<svg xmlns="http://www.w3.org/2000/svg">\n<style>rect{fill:#333}</style>\n'
+const tail = '</svg>\n'
+
+const rect = (x, y, w) => `<rect x="${x}" y="${y}" width="${w}" height="5" />\n`
+
+const hieroglyph = function (options) {
+	options = options || {}
+	let lineNr = -1
+
+	return connect( // todo: seems to be unelegant
+		lineLength(options),
+		through.obj(function (line, _, cb) {
+			lineNr++
+			if (line[1] === 0) cb(null, '')
+			else cb(null, rect(line[0], lineNr * 10, line[1]))
+		}),
+		wrap(head, tail)
+	)
+}
+
+
+
+Object.assign(hieroglyph, {head: head, tail: tail, rect: rect})
+module.exports = hieroglyph
